3c3088249fbd75d512591a9a3c18a2cc07a1b692
dam 2 <dam2@aulacampus.com>
2017-01-09 09:54:39 +0100

    no message

---
+++ src/Juego.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Juego {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		new QuienEsQuienEstructura();
+		
+	}
+
+
+}
